4cc66ca1e554982a4f0181de8a7ca825cdae2b4c
GulAli <a.aligul@hotmail.com>
2023-10-26 21:09:31 +0300

    User entity has been added.

---
+++ src/main/java/com/project/questapp/entities/User.java
@@ -0,0 +1,42 @@
+package com.project.questapp.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+/**********************************************
+    -HINTS-
+    1. By using "Entity" anotation, we indicate that
+    "User" class will be mapped to database.
+
+    2. By using "Table" anotation, we indicate that
+    a table named "user" will be created in our database.
+
+    3. "id" varible is a must as we create a table in
+    our database. That is why we use "Id" anotation. "id"
+    helps us add/delete/update our members in our table.
+    
+    Example PostgreSQL table:
+    
+    postgres=# SELECT * FROM user;
+    
+    id |  name  | surname
+    ----+--------+---------
+      1 | John   | Doe
+      2 | Jane   | Smith
+
+    4. "Data" anotation is used getter and setter methods
+    to automatically be written by Lombok dependency.
+
+**********************************************/ 
+
+@Entity
+@Table(name="user")
+@Data
+public class User {
+    @Id
+    private Long id;
+    private String userName;
+    private String password;
+}
