5639607e6a105e473351ed3deeae0c906e8d3326
GulAli <a.aligul@hotmail.com>
2023-10-29 17:47:56 +0300

    Addition of some controllers and other stuffs.

--- src/main/java/com/project/questapp/repos/PostRepository.java
+++ src/main/java/com/project/questapp/repos/PostRepository.java
@@ -1,9 +1,20 @@
 package com.project.questapp.repos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.questapp.entities.Post;
 
+/**********************************************
+    -HINTS-
+    1. "findBy..." is constant. Methods that start
+    with "findBy" is mapped to related methods in
+    JpaRepository interface.
+**********************************************/ 
+
 public interface PostRepository extends JpaRepository<Post, Long>{
+
+    List<Post> findByUserId(Long userId);
     
 }
